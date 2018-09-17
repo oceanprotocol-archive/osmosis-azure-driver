3e072e0005a94b52b02944d81b729a3881ee6e3a
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-09-17 10:51:30 +0200

    Azure token app login

--- tests/test_data_plugin.py
+++ tests/test_data_plugin.py
@@ -3,21 +3,22 @@ from osmosis_azure_driver.data_plugin import Plugin
 from osmosis_azure_driver.data_plugin import _parse_url
 import os
 
-# osmo = Osmosis('./tests/osmosis.ini').data_plugin()
-#
-#
-# def test_copy_file():
-#     assert osmo.type() == 'Azure'
+osmo = Osmosis('./tests/osmosis.ini').data_plugin()
+
+
+def test_copy_file():
+    assert osmo.type() == 'Azure'
+
 
 # To run this test you need to login with your credentials through az login
-# def test_list():
-#     pl = Plugin(resource_group_name='OceanProtocol')
-#     pl.upload('./LICENSE', 'https://testocnfiles.blob.core.windows.net/ocn-hackaton/license')
-#     pl.download('https://testocnfiles.blob.core.windows.net/ocn-hackaton/license', 'license')
-#     assert open('license').read() == open('./LICENSE').read()
-#     print(pl.list('ocn-hackaton','testocnfiles'))
-#     pl.delete('https://testocnfiles.blob.core.windows.net/ocn-hackaton/license')
-#     os.remove('license')
+def test_list():
+    pl = Plugin(resource_group_name='OceanProtocol')
+    pl.upload('./LICENSE', 'https://testocnfiles.blob.core.windows.net/ocn-hackaton/license_copy')
+    pl.download('https://testocnfiles.blob.core.windows.net/ocn-hackaton/license', 'license_copy')
+    assert open('license_copy').read() == open('./LICENSE').read()
+    print(pl.list('ocn-hackaton','testocnfiles'))
+    pl.delete('https://testocnfiles.blob.core.windows.net/ocn-hackaton/license_copy')
+    os.remove('license_copy')
 
 
 def test_split_url():
@@ -26,3 +27,7 @@ def test_split_url():
     assert parse_url.account == 'testocnfiles'
     assert parse_url.container == 'mycontainer'
     assert parse_url.blob == 'myblob'
+
+
+test_list()
+test_split_url()
