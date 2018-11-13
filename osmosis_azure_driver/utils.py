149d1cd44f8499f617050dcda90ffe0603d028e0
eruizgar91 <enrique@oceanprotocol.com>
2018-11-13 10:49:13 +0100

    Move computing from Brizo to osmosis azure driver

---
+++ osmosis_azure_driver/utils.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+
+azure_parameters = namedtuple('Azure', ['account', 'file_type', 'container_or_share_name', 'path', 'file'])
+
+
+def _parse_url(url):
+    account = url[8:].split('/')[0].split('.')[0]
+    file_type = url[8:].split('/')[0].split('.')[1]
+    # TODO Modify to manage directory trees.
+    container_or_share_name = url[8:].split('/')[1]
+    path = None if url[8:].split('/')[2] == url[8:].split('/')[-1] else url[8:].split('/')[2]
+    file = url[8:].split('/')[-1]
+    # if account != self.account:
+    #     self.logger.error('This url has a wrong account.')
+    #     raise OsmosisError
+
+    return azure_parameters(account, file_type, container_or_share_name, path, file)
