840b3446624be8b24b0b17cd675f58193a265860
eruizgar91 <enrique@oceanprotocol.com>
2019-01-23 15:55:37 +0100

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/osmosis-azure-driver',
-    version='0.0.2',
+    version='0.0.3',
     zip_safe=False,
 )
